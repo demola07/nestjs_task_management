1b84863a90702c86259a320a696d0f83f541db0b
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-27 23:36:05 +0100

    create a data transfer object

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v1 as uuidv1 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,9 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createtaskDto: CreateTaskDto): Task {
+    const { title, description } = createtaskDto;
+
     const task: Task = {
       id: uuidv1(),
       title,
