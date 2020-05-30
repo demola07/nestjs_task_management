fb4ee1c9023c0dfe629875a0c702baf7d62ee996
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-30 10:07:39 +0100

    persist getting tasks and filter using typeorm and the querybuilder

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,6 +2,7 @@ import { Task } from './task.entity';
 import { Repository, EntityRepository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
@@ -16,5 +17,23 @@ export class TaskRepository extends Repository<Task> {
     await task.save();
 
     return task;
+  }
+
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+    if (search) {
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = query.getMany();
+    return tasks;
   }
 }
