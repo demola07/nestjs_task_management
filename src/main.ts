8b7c62853e5276bb98bfa331da0487ad771cd25e
demola07 <45898468+demola07@users.noreply.github.com>
2020-06-01 00:21:35 +0100

    big fix with environmental variables

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
-
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const serverConfig = config.get('server');
 
   const PORT = process.env.PORT || serverConfig.port;
   await app.listen(PORT);
