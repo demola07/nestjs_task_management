f7861f1d543061a15dd0470371258ea28bf668ad
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-31 11:32:09 +0100

    authorization - deleting a user's task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -63,7 +63,10 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
-    return this.tasksService.deleteTask(id);
+  deleteTask(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
   }
 }
